ea71449666ae0e94013828b4ca9f4c4741b1638e
migas06 <miguelmartins018@gmail.com>
2016-12-24 01:46:43 +0000

    Accelerometer Functional II

--- Tetris/app/src/main/java/com/isec/tetris/GameActivity.java
+++ Tetris/app/src/main/java/com/isec/tetris/GameActivity.java
@@ -3,12 +3,17 @@ package com.isec.tetris;
 import android.app.Activity;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Display;
+import android.widget.Toast;
 
 public class GameActivity extends Activity {
 
     TetrisGridView tetrisGrid;
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +24,17 @@ public class GameActivity extends Activity {
         Point point = new Point();
         display.getSize(point);
 
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (sensor == null) {
+            Toast.makeText(this, getResources().getString((R.string.sensor_error)),
+                    Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
 
-        tetrisGrid = new TetrisGridView(this, point.x, point.y);
-        tetrisGrid.setZOrderOnTop(true);
-        tetrisGrid.getHolder().setFormat(PixelFormat.TRANSPARENT);
+        tetrisGrid = new TetrisGridView(this, point.x, point.y, sensor, sensorManager);
+
         setContentView(tetrisGrid);
     }
 
@@ -37,4 +49,7 @@ public class GameActivity extends Activity {
         super.onPause();
         tetrisGrid.onPause();
     }
+
+
+
 }
